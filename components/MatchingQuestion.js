5d4df5c905331533a76c8e6bdca1358e9619538a
Dan Kennedy <dannykennedy@email.com>
2023-11-04 21:24:00 +1100

    feat: working drag and drop

--- components/MatchingQuestion.js
+++ components/MatchingQuestion.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import styles from "../styles/MatchingQuestion.module.css";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Image from "next/image";
@@ -27,18 +27,118 @@ const MatchingQuestionItem = ({ item, index }) => {
 };
 
 export const MatchingQuestion = ({ question }) => {
-  const [sourceItems, setSourceItems] = useState(question.set1);
-  const [answerBoxes, setAnswerBoxes] = useState(
-    question.set2.map((x) => ({ ...x, sourceItems: [] }))
-  );
+  // STRUCTURE THE DATA
+  const sourceItems = question.set1.reduce((acc, item) => {
+    acc[item.key] = item;
+    return acc;
+  }, {});
+  const sourceBox = {
+    id: `questions`,
+    title: `Questions`,
+    itemKeys: question.set1.map((x) => x.key),
+  };
+  const answerBoxes = question.set2.map((item, index) => {
+    return {
+      id: `${item.key}`,
+      title: item.title,
+      itemKeys: [],
+    };
+  });
+  const allBoxes = [sourceBox, ...answerBoxes];
 
+  // STATE
+  const [boxes, setBoxes] = useState(allBoxes);
+
+  const setNewBoxes = (newBoxes) => {
+    setBoxes(newBoxes);
+  };
+
+  // FUNCTIONS
   const onDragEnd = (result) => {
-    console.log("result", result);
-    if (!result.destination) return;
-    const items = Array.from(sourceItems);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    setSourceItems(items);
+    const { source, destination, draggableId } = result;
+
+    // If the item is dropped outside of a box, do nothing
+    if (!destination) {
+      return;
+    }
+
+    const sourceBox = allBoxes.find((x) => x.id === source.droppableId);
+    const destBox = allBoxes.find((x) => x.id === destination.droppableId);
+
+    // 1) We put the box back where it was
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    // 2) If the item is reordered within the same box
+    else if (destination.droppableId === source.droppableId) {
+      const newBoxes = [...boxes].map((box) => {
+        if (box.id === sourceBox.id) {
+          const itemKeys = reorder(
+            box.itemKeys,
+            draggableId,
+            destination.index
+          );
+          return {
+            ...box,
+            itemKeys,
+          };
+        } else {
+          return box;
+        }
+      });
+      setNewBoxes(newBoxes);
+    }
+    // 3) If the item is moved to a different box
+    else {
+      const newBoxes = [...boxes].map((box) => {
+        if (box.id === sourceBox.id) {
+          const newItemKeys = remove(box.itemKeys, draggableId);
+          return {
+            ...box,
+            itemKeys: newItemKeys,
+          };
+        } else if (box.id === destBox.id) {
+          const itemKeys = insert(box.itemKeys, draggableId, destination.index);
+          return {
+            ...box,
+            itemKeys,
+          };
+        } else {
+          return box;
+        }
+      });
+      setNewBoxes(newBoxes);
+    }
+  };
+
+  // Reordering within the box
+  const reorder = (keyList, itemKey, endIndex) => {
+    const currentIndex = keyList.indexOf(itemKey);
+    // If the item is not in the list, log and return
+    if (currentIndex === -1) {
+      console.log("Item not in list");
+      return;
+    }
+    const resultKeyList = [...keyList];
+    const keyListWithoutItem = resultKeyList.filter((x) => x !== itemKey);
+    keyListWithoutItem.splice(endIndex, 0, itemKey);
+    return keyListWithoutItem;
+  };
+
+  // Adding an item to the box
+  const insert = (keyList, itemKey, endIndex) => {
+    const resultKeyList = [...keyList];
+    resultKeyList.splice(endIndex, 0, itemKey);
+    return resultKeyList;
+  };
+
+  // Remove an item from a box
+  const remove = (keyList, itemKey) => {
+    const resultKeyList = [...keyList];
+    return resultKeyList.filter((x) => x !== itemKey);
   };
 
   return (
@@ -51,44 +151,47 @@ export const MatchingQuestion = ({ question }) => {
           onDragEnd(result);
         }}
       >
-        <Droppable
-          droppableId={`droppable-questions`}
-          key={`droppable-questions`}
-        >
-          {(provided) => {
-            return (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className={styles["matching-question-questions-box"]}
-              >
-                {sourceItems.map((item, index) => {
-                  return (
-                    <Draggable
-                      key={item.key}
-                      draggableId={item.key}
-                      index={index}
-                    >
-                      {(provided) => {
-                        return (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >
-                            <MatchingQuestionItem item={item} index={index} />
-                          </div>
-                        );
-                      }}
-                    </Draggable>
-                  );
-                })}
-                {/* Makes the box bigger when dropping */}
-                {provided.placeholder}
-              </div>
-            );
-          }}
-        </Droppable>
+        <div className={styles["matching-question__container"]}>
+          {boxes.map((box, index) => (
+            <div key={box.id} className={styles["matching-question__box"]}>
+              <h3>{box.title}</h3>
+              <Droppable droppableId={box.id}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className={styles["matching-question__box__content"]}
+                  >
+                    {box.itemKeys.map((itemKey, index) => {
+                      const item = sourceItems[itemKey];
+                      return (
+                        <Draggable
+                          key={item.key}
+                          draggableId={item.key}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className={
+                                styles["matching-question__box__content__item"]
+                              }
+                            >
+                              <MatchingQuestionItem item={item} />
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </div>
+          ))}
+        </div>
       </DragDropContext>
     </div>
   );
